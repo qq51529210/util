f7683b84835749d2fbebd509ba958ff9f5972834
LinWenBin <51529210@qq.com>
2021-02-09 20:10:39 +0800

    add

---
+++ zigzag.go
@@ -0,0 +1,18 @@
+// 整数的压缩算法
+package common
+
+func ZigZag32(n int32) int32 {
+	return (n >> 1) ^ (n << 31)
+}
+
+func ZigZag64(n int64) int64 {
+	return (n >> 1) ^ (n << 63)
+}
+
+func FromZigZag32(n int32) int32 {
+	return int32(uint32(n)>>1) ^ -(n & 1)
+}
+
+func FromZigZag64(n int64) int64 {
+	return int64(uint64(n)>>1) ^ -(n & 1)
+}
