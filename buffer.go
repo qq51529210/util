fd1f96502de3a2cc9079e47fbf9599c9e34d55bb
LinWenBin <51529210@qq.com>
2021-05-31 23:39:54 +0800

    u

--- buffer.go
+++ buffer.go
@@ -22,92 +22,96 @@ func init() {
 
 // Use for read a segmented message.
 type readBuffer struct {
-	b []byte
-	n int
+	buf []byte
+	len int
 }
 
 // Make sure b can stores n bytes data.
 func (b *readBuffer) grow(n int) {
-	m := b.n + n
-	if m > len(b.b) {
+	m := b.len + n
+	if m > len(b.buf) {
 		nb := make([]byte, m)
-		copy(nb, b.b)
-		b.b = nb
+		copy(nb, b.buf)
+		b.buf = nb
 	}
+}
+
+func (b *readBuffer) Reset() {
+	b.len = 0
 }
 
 func (b *readBuffer) ReadN(r io.Reader, n int) error {
 	b.grow(n)
-	n, err := io.ReadFull(r, b.b[b.n:b.n+n])
+	n, err := io.ReadFull(r, b.buf[b.len:b.len+n])
 	if err != nil {
 		return err
 	}
-	b.n += n
+	b.len += n
 	return nil
 }
 
 // Use for encode message.
 type encodeBuffer struct {
-	b []byte
-	n int
+	buf []byte
+	len int
 }
 
 func (b *encodeBuffer) Reset() {
-	b.n = 0
+	b.len = 0
 }
 
 // Make sure b can stores n bytes data.
 func (b *encodeBuffer) grow(n int) {
-	m := b.n + n
-	if m > len(b.b) {
+	m := b.len + n
+	if m > len(b.buf) {
 		nb := make([]byte, m)
-		copy(nb, b.b)
-		b.b = nb
+		copy(nb, b.buf)
+		b.buf = nb
 	}
 }
 
 func (b *encodeBuffer) Put8(n byte) {
 	b.grow(1)
-	b.b[b.n] = n
-	b.n++
+	b.buf[b.len] = n
+	b.len++
 }
 
 // Append BigEndian n
 func (b *encodeBuffer) Put16(n uint16) {
 	b.grow(2)
-	binary.BigEndian.PutUint16(b.b[b.n:], n)
-	b.n += 2
+	binary.BigEndian.PutUint16(b.buf[b.len:], n)
+	b.len += 2
 }
 
 // Append BigEndian n
 func (b *encodeBuffer) Put32(n uint32) {
 	b.grow(4)
-	binary.BigEndian.PutUint32(b.b[b.n:], n)
-	b.n += 4
+	binary.BigEndian.PutUint32(b.buf[b.len:], n)
+	b.len += 4
 }
 
 // Append BigEndian n
 func (b *encodeBuffer) Put64(n uint64) {
 	b.grow(8)
-	binary.BigEndian.PutUint64(b.b[b.n:], n)
-	b.n += 8
+	binary.BigEndian.PutUint64(b.buf[b.len:], n)
+	b.len += 8
 }
 
 // Append n random bytes.
 func (b *encodeBuffer) PutRandom(n int) {
 	b.grow(n)
-	random.Read(b.b[b.n:])
-	b.n += n
+	random.Read(b.buf[b.len:])
+	b.len += n
 }
 
 // Append bytes.
 func (b *encodeBuffer) PutBytes(d []byte) {
 	b.grow(len(d))
-	b.n += copy(b.b[b.n:], d)
+	b.len += copy(b.buf[b.len:], d)
 }
 
 // Append string.
 func (b *encodeBuffer) PutString(s string) {
 	b.grow(len(s))
-	b.n += copy(b.b[b.n:], s)
+	b.len += copy(b.buf[b.len:], s)
 }
