3c5901d33a40291a4fa6a4b31d3525dfd6047aa2
qq51529210 <51529210@qq.com>
2023-05-15 23:32:15 +0800

    u

---
+++ cfg.go
@@ -0,0 +1,79 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+// ReadCfg 读取第一个运行参数
+func ReadCfg(uri string, ptr any) error {
+	_u, err := url.Parse(uri)
+	if err != nil {
+		return err
+	}
+	switch _u.Scheme {
+	case "http", "https":
+		return readCfgFromHTTP(uri, ptr)
+	default:
+		ext := filepath.Ext(uri)
+		switch ext {
+		case ".json":
+			return readCfgJSON(uri, ptr)
+		case ".yaml", ".yml":
+			return readCfgYAML(uri, ptr)
+		default:
+			return fmt.Errorf("unsupported config type %s", ext)
+		}
+	}
+}
+
+func readCfgFromHTTP(uri string, ptr any) error {
+	// 下载
+	res, err := http.Get(uri)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	// 看看是什么
+	contentType := res.Header.Get("Content-Type")
+	if strings.Contains(contentType, "application/json") {
+		// 解析
+		return json.NewDecoder(res.Body).Decode(ptr)
+	}
+	if strings.Contains(contentType, "application/yaml") ||
+		strings.Contains(contentType, "application/x-yaml") {
+		// 解析
+		return yaml.NewDecoder(res.Body).Decode(ptr)
+	}
+	//
+	return fmt.Errorf("unsupported content type %s", contentType)
+}
+
+func readCfgJSON(uri string, ptr any) error {
+	// 打开文件
+	file, err := os.Open(uri)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	// 解析
+	return json.NewDecoder(file).Decode(ptr)
+}
+
+func readCfgYAML(uri string, ptr any) error {
+	// 打开文件
+	file, err := os.Open(uri)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	// 解析
+	return yaml.NewDecoder(file).Decode(ptr)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/qq51529210/util
 
 go 1.19
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
