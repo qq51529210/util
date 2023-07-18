2ab4c3d3554f7ce7134ff99a569aaf2c80243e49
LinWenBin <51529210@qq.com>
2023-07-18 17:56:42 +0800

    u

--- gin.go
+++ gin.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"io/fs"
 	"net/http"
+	"path"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -15,25 +16,27 @@ import (
 	zht "github.com/go-playground/validator/v10/translations/zh"
 )
 
+const indexHTML = "/index.html"
+
 // GinStaticDir 初始化静态文件
-func GinStaticDir(r gin.IRouter, dir fs.FS) (err error) {
-	index := "/index.html"
-	return fs.WalkDir(dir, ".", func(path string, d fs.DirEntry, err error) error {
+func GinStaticDir(r gin.IRouter, relativeRoot, staticsRoot string, staticsDir fs.FS) (err error) {
+	return fs.WalkDir(staticsDir, ".", func(p string, d fs.DirEntry, err error) error {
 		if d == nil || d.IsDir() {
 			return nil
 		}
-		r.StaticFileFS(path, path, http.FS(dir))
-		if strings.HasSuffix(path, index) {
-			ginStaticIndex(r, dir, path, index)
+		rp := path.Join(relativeRoot, strings.TrimPrefix(p, staticsRoot))
+		r.StaticFileFS(rp, p, http.FS(staticsDir))
+		if strings.HasSuffix(p, indexHTML) {
+			ginStaticIndex(r, staticsDir, rp, p)
 		}
 		return nil
 	})
 }
 
 // 以免 gin 内部对 index.html 一直重定向
-func ginStaticIndex(r gin.IRouter, statics fs.FS, path, index string) {
-	r.GET(path[:len(path)-len(index)], func(ctx *gin.Context) {
-		f, err := statics.Open(path)
+func ginStaticIndex(r gin.IRouter, statics fs.FS, path, filePath string) {
+	r.GET(path[:len(path)-len(indexHTML)], func(ctx *gin.Context) {
+		f, err := statics.Open(filePath)
 		if err != nil {
 			ctx.Status(http.StatusNotFound)
 			return
