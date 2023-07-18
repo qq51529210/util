e00488e18d7e34a0f10d7d0f687e382a68fb1223
LinWenBin <51529210@qq.com>
2023-07-18 14:25:52 +0800

    u

--- gin.go
+++ gin.go
@@ -1,16 +1,22 @@
 package util
 
 import (
+	"fmt"
 	"io"
 	"io/fs"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zht "github.com/go-playground/validator/v10/translations/zh"
 )
 
-// InitGinStaticDir 初始化静态文件
-func InitGinStaticDir(r gin.IRouter, dir fs.FS) (err error) {
+// GinStaticDir 初始化静态文件
+func GinStaticDir(r gin.IRouter, dir fs.FS) (err error) {
 	index := "/index.html"
 	return fs.WalkDir(dir, ".", func(path string, d fs.DirEntry, err error) error {
 		if d == nil || d.IsDir() {
@@ -18,14 +24,14 @@ func InitGinStaticDir(r gin.IRouter, dir fs.FS) (err error) {
 		}
 		r.StaticFileFS(path, path, http.FS(dir))
 		if strings.HasSuffix(path, index) {
-			initGinStaticIndex(r, dir, path, index)
+			ginStaticIndex(r, dir, path, index)
 		}
 		return nil
 	})
 }
 
 // 以免 gin 内部对 index.html 一直重定向
-func initGinStaticIndex(r gin.IRouter, statics fs.FS, path, index string) {
+func ginStaticIndex(r gin.IRouter, statics fs.FS, path, index string) {
 	r.GET(path[:len(path)-len(index)], func(ctx *gin.Context) {
 		f, err := statics.Open(path)
 		if err != nil {
@@ -37,4 +43,24 @@ func initGinStaticIndex(r gin.IRouter, statics fs.FS, path, index string) {
 		//
 		io.Copy(ctx.Writer, f)
 	})
+}
+
+// GinValidateZH 验证器设置为中文
+func GinValidateZH(errs map[string]string) {
+	if va, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		// 设置
+		lt := zh.New()
+		_ut := ut.New(lt, lt)
+		t, _ := _ut.GetTranslator("zh")
+		zht.RegisterDefaultTranslations(va, t)
+		//
+		for k, v := range errs {
+			va.RegisterTranslation(k, t, func(ut ut.Translator) error {
+				return ut.Add(k, fmt.Sprintf("{0}%s", v), true)
+			}, func(ut ut.Translator, fe validator.FieldError) string {
+				t, _ := ut.T(k, fe.Field())
+				return t
+			})
+		}
+	}
 }
